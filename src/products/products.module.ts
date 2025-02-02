2cd044291cf8e8ed45676484645ed248a49b2c05
Guilhermy Rodrigues <guilhermyr87@gmail.com>
2025-02-01 22:24:32 -0300

    validando slug

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,30 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
 })
-export class ProductsModule {}
+export class ProductsModule implements OnModuleInit{
+  constructor(
+    private prismaService: PrismaService, 
+    private productService: ProductsService
+  ) {}
+
+  async onModuleInit() {
+    const products = new Array(10).fill(0).map((_, index) => index + 1);
+
+    await this.prismaService.product.deleteMany();
+
+    for (const productIndex of products) {
+      await this.productService.create({
+        name: `Product ${productIndex}`,
+        slug: `product-${productIndex}`,
+        description: `Description of ${productIndex}`,
+        price: productIndex * 100,
+      })
+    }
+  }
+}
